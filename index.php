63064ccd5592ac7d9d464930cb9763f6ab834b02
damir_dev <damirkap89@gmail.com>
2023-12-04 17:22:41 +0600

    type of class

--- index.php
+++ index.php
@@ -26,9 +26,30 @@ public function sayHi(){
  public static function Smth(){
     echo 'gogogoo';
  }
+
 }
 
-Person::Smth();
+// Person::Smth();
+
+
+class Student{
+    public $name = 'I am student ';
+    public $age=90;
+}
+class Univercity{
+    public $students;
+
+
+    public function addStudent(Student $student){
+        echo $student->name . ' ' . 'and my age - ' . $student->age;    }
+}
+
+$univer = new Univercity;
+
+$person = new Person('Zhan', 24);
+$student = new Student('Abay', 22);
+// echo $student->name . ' ' . 'and my age - ' . $student->age;
+$univer -> addStudent($student);
 
 
  
