78de8f89f17bec97f4760115b2b0b78556f0b249
damir_dev <damirkap89@gmail.com>
2024-01-01 14:13:46 +0600

    just PHP

--- index.php
+++ index.php
@@ -1,55 +1,86 @@
 <?php
+$posts = 
+  [
+    [
+        "id" => 1,
+        "title" => "lorem"
+    ],
+    [
+        "id" => 2,
+        "title" => "lorem2"
+    ],
+    [
+        "id" => 3,
+        "title" => "lorem3"
+    ],
 
-class Person{
-    public $age;
-    public $name;
-
-public function __construct($name, $age){
-    $this->name = $name;
-    $this->age = $age;
-}
-public function sayHi(){
-    return 'Hi I am ' . $this->name . ' ' . 'and my age: ' . $this->age;
-}
- public function setName($name){      
-    $this->name = $name;
- }
-
- public function setAge($age){
-    $this->age=$age;
- }
-
- public function sayAge(){
-     return $this->age;
- }
-
- public static function Smth(){
-    echo 'gogogoo';
- }
-
-}
-
-// Person::Smth();
+]
 
 
-class Student{
-    public $name = 'I am student ';
-    public $age=90;
-}
-class Univercity{
-    public $students;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1></h1>
+</body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">My Blog</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">MainPage</a>
+            </li>
+
+        </ul>
+
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8-offset-md-2">
+            <a href="#" class="btn btn-success">Add Post</a>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($posts as $post) : ?>
+                        <tr>
+                            <th scope="row"><?= $post['id']?></th>
+                            <td><?= $post['title']?> </td>
+                            <td>
+                                <a href="#" class="btn btn-warning">Edit</a>
+                                <a href="#" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
-    public function addStudent(Student $student){
-        echo $student->name . ' ' . 'and my age - ' . $student->age;    }
-}
-
-$univer = new Univercity;
-
-$person = new Person('Zhan', 24);
-$student = new Student('Abay', 22);
-// echo $student->name . ' ' . 'and my age - ' . $student->age;
-$univer -> addStudent($student);
-
-
- 
+</html>
