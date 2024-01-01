6add78f638f5c1d1b0ecc3f30469d0efc6f752ae
damir_dev <damirkap89@gmail.com>
2024-01-01 21:40:08 +0600

    with construct

--- db/QueryBuilder.php
+++ db/QueryBuilder.php
@@ -2,19 +2,21 @@
 
 class QueryBuilder
 {
-   // protected $pdo;
-   // public function __construct($pdo)
-   // {
-   //    $this->$pdo = $pdo;
-   // }
-   // $db = new QueryBuilder(pdo);
-   function getAll($pdo){   
+   protected $pdo;
+
+   public function __construct($pdo)
+   {
+      $this->pdo = $pdo;
+   }
+
+   // $db = new QueryBuilder();
+   function getAll()
+   {
       $sql = 'SELECT * FROM posts';
-      $stmt = $pdo->prepare($sql);
+      $stmt = $this->pdo->prepare($sql);
       $stmt->execute();
 
       $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
       return $posts;
-
    }
 }
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 include 'functions.php';
 include 'db/QueryBuilder.php';
-$pdo = connect_to();
 
-$db = new QueryBuilder;
-$posts = $db->getAll($pdo);
+$pdo = connect_to();
+$db = new QueryBuilder($pdo);
+
+$posts = $db->getAll();
 
 
 include  'index.view.php';
